c5629da97ca770cd7bc27e7447fee04185a8fed7
RSKonkle <rskonkle@gmail.com>
2024-12-23 20:45:01 -0500

    Create skeleton functions for add, sub, mult, div.

---
+++ script.js
@@ -0,0 +1,27 @@
+/* 
+    OBJECTIVES: FIVE TOTAL FUNCTIONS
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, OPERATE (EQUALS)
+    CREATE REACTIVE DISPLAY
+    ADD NUMBER KEYS
+    CSS FORMATTING
+*/
+
+function addition(x, y){
+    return 0;
+}
+
+function subtraction(x, y){
+    return 0;
+}
+
+function multiplication(x, y){
+    return 0;
+}
+
+function division(x, y){
+    return 0;
+}
+
+function operate(){
+    return 0;
+}
